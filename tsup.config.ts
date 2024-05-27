e3012b00c9cb2fbc2ddd8def7266a31469bc0026
adityaa72 <adityah1357908642@gmail.com>
2024-05-27 17:42:09 +0530

    fix: module type

---
+++ tsup.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src"],
+  minify: true,
+  format: ["esm"],
+  clean: true,
+  esbuildOptions(options) {
+    options.banner = {
+      js: '"use client"',
+    };
+  },
+});
